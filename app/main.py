4dcbbb917ac1f1395fa1d2615354672eb49e0be3
Agnieszka Barcik <aga.barcik11@gmail.com>
2026-03-05 15:19:19 -0500

    Address review: validate current_load in init

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ class Cargo:
 
 class BaseRobot:
     def __init__(
-            self, name: str,
+            self,
+            name: str,
             weight: int,
             coords: list[int] | None = None
     ) -> None:
@@ -33,7 +34,8 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(
-            self, name: str,
+            self,
+            name: str,
             weight: int,
             coords: list[int] | None = None
     ) -> None:
@@ -51,15 +53,18 @@ class FlyingRobot(BaseRobot):
 
 class DeliveryDrone(FlyingRobot):
     def __init__(
-            self, name: str,
+            self,
+            name: str,
             weight: int,
             max_load_weight: int,
-            current_load: Cargo = None,
+            current_load: Cargo | None = None,
             coords: list[int] | None = None
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = current_load
+        self.current_load = None
+        if current_load is not None:
+            self.hook_load(current_load)
 
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
